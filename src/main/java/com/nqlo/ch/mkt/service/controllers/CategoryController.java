d788758d3559e479af5bdc7d5a619bd951b3e710
niqloma <nicolomanto@gmail.com>
2025-01-30 16:24:38 -0300

    SWAGGER implemented on CATEGORIES CONTROLLER

--- src/main/java/com/nqlo/ch/mkt/service/controllers/CategoryController.java
+++ src/main/java/com/nqlo/ch/mkt/service/controllers/CategoryController.java
@@ -22,6 +22,11 @@ import com.nqlo.ch.mkt.service.exceptions.DuplicateEntryException;
 import com.nqlo.ch.mkt.service.exceptions.ResourceNotFoundException;
 import com.nqlo.ch.mkt.service.services.CategoryService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 
 @RestController
@@ -31,6 +36,16 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
+    @Operation(summary = "Get Categories List")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Categories retrieved successfully",
+                content = {
+                    @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Category.class))}),
+        @ApiResponse(responseCode = "500", description = "Internal server error",
+                content = {
+                    @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class))}),})
     @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Category>> getAllCategories() {
         try {
@@ -43,12 +58,24 @@ public class CategoryController {
         }
     }
 
+    @Operation(summary = "Get Category by ID")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Category retrieved successfully", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = Category.class)) }),
+			@ApiResponse(responseCode = "404", description = "Category not found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+			@ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))) })
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Category> getCategoryById(@PathVariable Long id) {
         Category category = categoryService.findById(id);
         return ResponseEntity.ok(category);
     }
 
+    @Operation(summary = "Create Category")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "201", description = "Category created successfully", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = Category.class)) }),
+			@ApiResponse(responseCode = "400", description = "Bad request", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+			@ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))) })
     @PostMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Object> createCategory(@Valid @RequestBody Category category, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
@@ -62,24 +89,37 @@ public class CategoryController {
         return ResponseEntity.status(HttpStatus.CREATED).body(newCategory); //201
     }
 
+    @Operation(summary = "Update Category")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Category updated successfully", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = Category.class)) }),
+            @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+			@ApiResponse(responseCode = "404", description = "Category not found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+			@ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))) })
     @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Object> updateCategory(@PathVariable Long id, @RequestBody Category category) {
         try {
             category.setId(id);
             Category updatedCategory = categoryService.updateById(id, category);
             return ResponseEntity.ok(updatedCategory);
-    } catch (DuplicateEntryException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(new ErrorResponse(e.getMessage(), "Bad Request", HttpStatus.BAD_REQUEST.value()));
+        } catch (DuplicateEntryException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new ErrorResponse(e.getMessage(), "Bad Request", HttpStatus.BAD_REQUEST.value()));
         } catch (ResourceNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(new ErrorResponse(e.getMessage(), "Not Found", HttpStatus.NOT_FOUND.value()));
-    } catch (Exception e) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new ErrorResponse("An unexpected error occurred: " + e.getMessage(), "Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR.value()));
-    }
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new ErrorResponse(e.getMessage(), "Not Found", HttpStatus.NOT_FOUND.value()));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ErrorResponse("An unexpected error occurred: " + e.getMessage(), "Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR.value()));
+        }
     }
 
+    @Operation(summary = "Delete Category by ID")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "204", description = "Category deleted successfully", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = Category.class)) }),
+			@ApiResponse(responseCode = "404", description = "Category not found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+			@ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))) })
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<?> deleteCategory(@PathVariable Long id) {
         try {
@@ -88,9 +128,9 @@ public class CategoryController {
         } catch (ResourceNotFoundException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body(new ErrorResponse(e.getMessage(), "Not Found", HttpStatus.NOT_FOUND.value()));
-                } catch (Exception e) {
-                    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                            .body(new ErrorResponse("An unexpected error occurred: " + e.getMessage(), "Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR.value()));
-                }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ErrorResponse("An unexpected error occurred: " + e.getMessage(), "Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR.value()));
+        }
     }
 }
